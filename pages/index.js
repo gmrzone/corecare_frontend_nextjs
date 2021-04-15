048025e1682f908e9706d1b169404520f77dcc0c
afzal <saiyedafzalgz@gmail.com>
2021-04-15 03:38:36 -0700

    fix error

--- pages/index.js
+++ pages/index.js
@@ -7,13 +7,57 @@ import SliderWrapper from '../components/common/SliderWrapper'
 import axios from 'axios'
 import Section from '../components/common/Section'
 import ReviewSection from '../components/home/ReviewSection'
+const reviewData = [
+  {
+      name: '@Bilalhashmi29',
+      msge: 'One Stop shop solution for all your need at your doorstep. staying focused on hygiene and sanatization <span class="company-name">@CoreCare</span>',
+      image: "/image1.jpg"
+  },
+  {
+      name: '@AdityaRPatro',
+      msge: `Brilliant Staff <span class="company-name">@afzalSaiyed</span> and to the entire <span class="company-name">@CoreCare</span> Team for stepping up duting uncertain future`,
+      image: '/image2.jpg'
+  },
+  {
+      name: '@Bhanu82284032',
+      msge: `<span class="company-name">@CoreCare</span> You guys rock did'nt expect such exceptional service even in this pandamic situation.`,
+      image: "/image3.jpg"
+  },
+  {
+      name: '@afzal_saiyed',
+      msge: `<span class="company-name">@CoreCare</span> I Appreciate the precautianary measure taken by your servicemen. They were aware of aarogya setup importance..`,
+      image: "/image4.jpg"
+
+  },
+  {
+      name: '@sankeyBoy',
+      msge: `Had a short circuit in the apartment Electrician visit scheduled in a flash now things are back to normal Thanks to <span class="company-name">@CoreCare</span>`,
+      image: "/image5.jpg"
+
+  },
+  {
+      name: '@dhanotiya30',
+      msge: `Definately <span class="company-name">@CoreCare</span> is very helpful in this pendemic situation they are providing a hygenic service even during this pandamic situation`,
+      image: "/image6.jpg"
+
+  },
+  {
+      name: '@zaidshaikh129',
+      msge: 'One Stop shop solution for all your need at your doorstep. staying focused on hygiene and sanatization <span class="company-name">@CoreCare</span>',
+      image: "/image1.jpg"
+  },
+  {
+      name: '@Arshadshaikh',
+      msge: `Brilliant Staff <span class="company-name">@afzalSaiyed</span> and to the entire <span class="company-name">@CoreCare</span> Team for stepping up duting uncertain future`,
+      image: "/image2.jpg"
+  }
+]
 export const getStaticProps = async () => {
   const res = await axios.get('https://www.afzalsaiyed.corecare.in/')
   const res1 = await axios.get('https://www.afzalsaiyed.corecare.in/coupons/')
   const res2 = await axios.get('https://www.afzalsaiyed.corecare.in/subcategory/3/cleaner/')
   const res3 = await axios.get('https://www.afzalsaiyed.corecare.in/subcategory/2/carpenter/')
   const res4 = await axios.get('https://www.afzalsaiyed.corecare.in/subcategory/1/electrician/')
-  const res5 = await axios.get('http://127.0.0.1:3000/api/reviews/')
 
 
   return {
@@ -23,7 +67,6 @@ export const getStaticProps = async () => {
       cleaner: res2.data,
       carpenter: res3.data,
       electrician: res4.data,
-      review: res5.data
     }
   }
 }
@@ -48,7 +91,7 @@ const Home = ({ services, coupons,  mobileNav, cleaner, carpenter, electrician, 
             <Section title="Electrician Services">
               <SliderWrapper data={electrician.filter(x => x.slug !== "electrician-consultation")} image={true} mobileNav={mobileNav}/>
             </Section>
-            <ReviewSection mobileNav={mobileNav} data={review}/>
+            <ReviewSection mobileNav={mobileNav} data={reviewData}/>
       </Layout>
     </>
   )
