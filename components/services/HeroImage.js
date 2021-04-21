32b440d927596376fd4192c9852986fa2d78ae90
afzal <saiyedafzalgz@gmail.com>
2021-04-21 05:47:10 -0700

    fix service Hero Image

--- components/services/HeroImage.js
+++ components/services/HeroImage.js
@@ -5,10 +5,10 @@ const HeroImage = (props) => {
     const renderBullet = props.bullets.map((x, i) => <p key={i} className={"item " + style.bullets_p}>{x}</p>)
 
     return (
-        <div className={style.hero_image_container} style={{background: `linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0) 50%, rgba(0,0,0,1) 100%), url(${props.src})`}}>
+        <div className={style.hero_image_container} style={{backgroundImage: `linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0) 50%, rgba(0,0,0,1) 100%), url(${props.src})`}}>
         <div className="ui container content">
             <div className={style.main_title}>
-                <h1 className={style.main_title_text}>{props.mainTitle}</h1>
+                <h1 className={style.main_title_text + " heavy"}>{props.mainTitle}</h1>
             </div>
             <div className={style.bullet_points}>
                 <div className="ui bulleted list">
