514cbf995cce36b51869adb53eabb855b71f164e
afzal <saiyedafzalgz@gmail.com>
2021-04-24 03:40:28 -0700

    fix signup Modal final

--- components/common/Layout.js
+++ components/common/Layout.js
@@ -8,10 +8,10 @@ const Layout = ({ children, HeroImage=null, mobileNav, heroProps, ServiceListMod
     return (
         <>  
             <div className="afzal-container">
-                {ServiceListModel && signUpActive && <ServiceListModel {...serviceListProps} mobileNav={mobileNav}/>}
+                {ServiceListModel && <ServiceListModel {...serviceListProps} mobileNav={mobileNav}/>}
                 {CategoryChangeModal && <CategoryChangeModal {...categoryChangeProps}/>}
                 <div id="model"></div>
-                <SignUpModel modelActive={signUpActive} closeSignup={() => setSignUpActive(false)}/>
+                {signUpActive && <SignUpModel modelActive={signUpActive} closeSignup={() => setSignUpActive(false)}/>}
                 <Header mobileNav={mobileNav} openSignup={() => setSignUpActive(true)}/>
                 {HeroImage && <HeroImage {...heroProps}/>}
                 <main>
