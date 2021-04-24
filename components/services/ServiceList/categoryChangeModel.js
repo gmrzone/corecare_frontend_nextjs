5db3b0dec1f40dbfa74e6e9bf1a04930300cefb8
afzal <saiyedafzalgz@gmail.com>
2021-04-24 03:09:20 -0700

    made category replacement Modal fully responsive

--- components/services/ServiceList/categoryChangeModel.js
+++ components/services/ServiceList/categoryChangeModel.js
@@ -1,30 +1,38 @@
 import Model from '../../common/Modal';
 import Link from 'next/link';
-// import { replaceCart } from '../../../../actions'
-// import { connect } from 'react-redux'
+import axios from '../../../data/backendApi'
+import { BaseCartContext } from '../../../context/basicCartContext'
+import { useContext } from 'react'
 const categoryChangeModel = (props) => {
+    const {category, replacementItem: service_id} = props
+    const { mutateBaseCart } = useContext(BaseCartContext)
     const replaceCart = () => {
         // props.clearCart()
         // props.addToCart(props.replacementItem, props.category)
-        props.replaceCart(props.replacementItem, props.category, props.closeModel, props.incrementReplacedService.current)
-
+        // props.replaceCart(props.replacementItem, props.category, props.closeModel, props.incrementReplacedService.current)
+        axios.get('cart/clear/')
+        .then(response => {
+            if (response.data.status === 'ok'){
+                axios.post('cart/add/', {service_id: service_id, category: category})
+                .then(response => {
+                    mutateBaseCart({...response.data}, false)
+                })
+            }
+        })
     }
     return (
         <Model header={props.header} active={props.modelActive} closeModel={() => props.closeModel()} zIndex="5000">
             <div className="ui message">
-                {/* <div className="header">
-                    Cart cannot contain items from multiple Category
-                </div> */}
                 <p>Cart cannot contain items from multiple Category. Either Complete the current Cart Order or Replace Your Cart with different Category.</p>
             </div>
             <div style={{textAlign: 'right'}}>
-                    <button className="ui positive right labeled icon button" onClick={replaceCart}>
-                        Replace Cart
-                        <i className="checkmark icon"></i>
-                    </button>
-                    <Link href="/cart">
-                        <a className="ui secondary button">Cart</a>
-                    </Link>
+                <button className="ui positive right labeled icon button" onClick={replaceCart}>
+                    Replace Cart
+                    <i className="checkmark icon"></i>
+                </button>
+                <Link href="/cart">
+                    <a className="ui secondary button">Cart</a>
+                </Link>
             </div>
         </Model>
     )
