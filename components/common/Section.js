2b1d30658f967a3144889de264cbd3008e32fc22
afzal <saiyedafzalgz@gmail.com>
2021-05-05 13:31:46 -0700

    remove slider margin

--- components/common/Section.js
+++ components/common/Section.js
@@ -2,10 +2,15 @@ const Section = ({ children, title, description }) => {
     return (
         <div className="section__wrapper">
             <div className="ui container section_divider">
-                <h3>{title}</h3>
+                <h3 className="slider-title">{title}</h3>
                 {description ? <p>{description}</p> : ""}
                 {children}
             </div>
+            <style jsx>{`
+                .slider-title {
+                    margin-bottom: 0px;
+                }
+            `}</style>
         </div>
     )
 }
