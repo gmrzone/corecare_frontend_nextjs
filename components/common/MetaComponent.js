0a1fd27c6fc0f39735318091fbfba9179eaff192
afzal <saiyedafzalgz@gmail.com>
2021-05-06 02:41:18 -0700

    improbe Head Component

--- components/common/MetaComponent.js
+++ components/common/MetaComponent.js
@@ -25,7 +25,7 @@ const MetaComponent = ({ title, name, description, url }) => {
             <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
             <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
             <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-            <link rel="manifest" href="/site.webmanifest"></link>
+            <link rel="manifest" href="/site.webmanifest.json"></link>
             <link rel="icon" href="/favicon.ico" />
             <title>{title}</title>
         </Head>
