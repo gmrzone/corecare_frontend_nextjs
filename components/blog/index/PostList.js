ce30f071c874603a905d17bda566be210256f7a1
afzal <saiyedafzalgz@gmail.com>
2021-06-03 14:22:13 -0700

    Added InfiniteLoader To Blog Post List

--- components/blog/index/PostList.js
+++ components/blog/index/PostList.js
@@ -1,19 +1,31 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useRef } from 'react'
 import { PostListPaginationContext } from '../../../context/PostListPaginationContext'
 import style from '../../../styles/blog/index.module.scss'
 import PostListItem from './PostListItem'
 const PostList = () => {
     const { nextPage, pages } = useContext(PostListPaginationContext)
-    // const renderPosts = postList.posts.map(x => {
-    //     return <PostListItem post={x} key={x.id + x.slug}/>
-    // })
+    const loaderRef = useRef()
+
+    useEffect(() => {
+        const options = {}
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(x => {
+                if (x.isIntersecting){
+                    nextPage()
+                    console.log("get_next_page")
+                }
+            })
+        }, options)
+        observer.observe(loaderRef.current)
+    }, [])
+
     return (
         <div className={style.post_list_container}>
             <div className={`ui link cards ${style.cards}`}>
                 {/* {renderPosts} */}
                 {pages}
             </div>
-            <div className={`ui active centered inline medium loader ${style.loader}`}></div>
+            <div className={`ui active centered inline medium loader ${style.loader}`} ref={loaderRef}></div>
         </div>
     )
 }
--- context/PostListPaginationContext.js
+++ context/PostListPaginationContext.js
@@ -6,21 +6,21 @@ import PostListItem from '../components/blog/index/PostListItem'
 const PostListPaginationContext = createContext()
 
 const Page = ({ pageNum, mobileNav, initialData }) => {
-
+    const init = pageNum === 1 ? { initialData } : {}
     const fetcher = (...args) => axios.get(...args).then(response => response.data)
-    const {data: postList} = useSWR(mobileNav ? `blog/posts/?page=${pageNum}&size=6` : `blog/posts/?page=${pageNum}`, fetcher, {initialData: initialData})
-    console.log(postList)
-    return postList.posts.map(x => <PostListItem post={x} key={x.id}/>)
+    const {data: postList} = useSWR(mobileNav ? `blog/posts/?page=${pageNum}&size=6` : `blog/posts/?page=${pageNum}`, fetcher, init)
+    return postList?.posts.map(x => <PostListItem post={x} key={x.id}/>) || null
 }
 
 const PostListPaginationProvider = ({ children, initialData, mobileNav }) => {
     const [pageCount, setPageCount] = useState(1)
     const pages = []
-    for (let i=1; i <= pageCount; i++){
+    for (let i = 1; i <= pageCount; i++){
         pages.push(<Page key={i} pageNum={i} mobileNav={mobileNav} initialData={initialData}/>)
     }
     const nextPage = () => {
-        setPageCount(s => ++s)
+        setPageCount(s => s+1)
+        console.log(pageCount, "pageCount")
     }
     return (
         <PostListPaginationContext.Provider  value={{ nextPage, pages }}>
