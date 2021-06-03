67db6cca3467dc0e284651540d28cdd9e17bb8cb
afzal <saiyedafzalgz@gmail.com>
2021-06-03 05:43:08 -0700

    added blog posts

--- components/blog/index/PostList.js
+++ components/blog/index/PostList.js
@@ -1,12 +1,17 @@
 import { useContext } from 'react'
 import { PostListPaginationContext } from '../../../context/PostListPaginationContext'
 import style from '../../../styles/blog/index.module.scss'
+import PostListItem from './PostListItem'
 const PostList = () => {
     const { postList, mutatePostList } = useContext(PostListPaginationContext)
-    console.log(postList)
+    const renderPosts = postList.posts.map(x => {
+        return <PostListItem post={x} key={x.id + x.slug}/>
+    })
     return (
         <div className={style.post_list_container}>
-
+            <div class="ui link cards">
+                {renderPosts}
+            </div>
         </div>
     )
 }
--- components/blog/index/PostListItem.js
+++ components/blog/index/PostListItem.js
@@ -1,10 +1,40 @@
 import style from '../../../styles/blog/index.module.scss'
-
-const PostListItem = () => {
+import Link from 'next/link'
+const PostListItem = ({ post }) => {
+    const {slug,  date_slug : {year, month, day}} = post
+    const renderBody = (str) => {
+        if ((str===null) || (str===''))
+        return false;
+    else
+        str = str.toString();
+    return str.replace( /(<([^>]+)>)/ig, '').substring(0, 100) + " ....";
+    }
     return (
-        <div className={style.post_item}>
-
-        </div>
+        <Link href={`blog/${year}/${month}/${day}/${slug}`}>
+            <div className="card">
+                <div className="image">
+                <img src={post.photo} />
+                </div>
+                <div className="content">
+                <div className="head">{post.title}</div>
+                <div className="meta">
+                    <a>{post.created}</a>
+                </div>
+                <div className="description">
+                    {renderBody(post.body)}
+                </div>
+                </div>
+                <div className="extra content">
+                <span className="right floated">
+                    by {post?.author?.username || post?.author?.first_name + " " + post?.author?.last_name || post.name}
+                </span>
+                <span>
+                <i className="eye icon"></i>
+                    200 Views
+                </span>
+                </div>
+            </div>
+        </Link>
     )
 }
 
