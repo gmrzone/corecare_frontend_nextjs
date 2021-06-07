3c4515ad34101f4d051ccd9b67f174ecdbf744d4
afzal <saiyedafzalgz@gmail.com>
2021-06-07 05:08:22 -0700

    Modified Sitemap Generator to include all Blog Post

--- sitemap/generate_sitemap.js
+++ sitemap/generate_sitemap.js
@@ -16,7 +16,8 @@ const pretty = require("prettier");
     return url_links;
   };
   const createDynamicSitemap = (
-    services
+    services,
+    posts
   ) => `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
             ${services
@@ -43,11 +44,23 @@ const pretty = require("prettier");
                 `;
               })
               .join("")}
+              ${posts.map(x => {
+                return `
+                    <url>
+                        <loc>${`${domain}/blog/${x.date_slug.year}/${x.date_slug.month}/${x.date_slug.day}/${x.slug}`}</loc>
+                        <changefreq>daily</changefreq>
+                        <lastmod>2021-05-23T12:14:44+00:00</lastmod>
+                        <priority>0.7</priority>
+                    </url>
+                `;
+              })}
         </urlset>
         `;
   const request = await fetch(backend_url);
   const services = await request.json();
-  const sitemap = pretty.format(createDynamicSitemap(services), {
+  const postRequest = await fetch(backend_url + '/blog/posts/')
+  const posts = await postRequest.json()
+  const sitemap = pretty.format(createDynamicSitemap(services, posts), {
     parser: "html",
   });
   fs.writeFileSync("public/sitemap.xml", sitemap);
