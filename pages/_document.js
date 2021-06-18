4418f4111578a97b7e708ad2f018e8514941c0e5
afzal <saiyedafzalgz@gmail.com>
2021-06-18 12:05:39 -0700

    Changed Fonts

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,8 @@ class CoreCareDocument extends Document {
                 <Head>
                     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" integrity="sha512-8bHTC73gkZ7rZ7vpqUQThUDhqcNFyYi2xgDgPDHc+GXVGHXq+xPjynxIopALmOPqzo9JZj0k6OqqewdGO3EsrQ==" crossOrigin="anonymous" key="symantic"/>
                     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-iKbFRxucmOHIcpWdX9NTZ5WETOPm0Goy0WmfyNcl52qSYtc2Buk0NCe6jU1sWWNB" crossOrigin="anonymous" key="fontawesome"/>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700;800&display=swap" rel="stylesheet"></link>
                 </Head>
                 <body>
                     <Main />
