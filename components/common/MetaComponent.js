e341a6af35b2f633e4f9c4ef48d193a97281fe41
afzal saiyed <saiyedafzalgz@gmail.com>
2021-07-04 20:30:23 +0530

    modified head

--- components/common/MetaComponent.js
+++ components/common/MetaComponent.js
@@ -28,8 +28,8 @@ const MetaComponent = ({ title, name, description="Corecare is the best cleaning
             <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
             <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
             <link rel="manifest" href="/site.webmanifest" />
-            <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-            <meta name="msapplication-TileColor" content="#da532c" />
+            <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#6c6c6c" />
+            <meta name="msapplication-TileColor" content="#2d89ef" />
             <meta name="theme-color" content="#ffffff" />
             <title>{title}</title>
         </Head>
