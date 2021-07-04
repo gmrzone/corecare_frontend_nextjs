448b6bf2225a0c774f925b8554f546d11bf89684
afzal saiyed <saiyedafzalgz@gmail.com>
2021-07-04 20:12:42 +0530

    modified head

--- components/common/MetaComponent.js
+++ components/common/MetaComponent.js
@@ -3,6 +3,17 @@ const MetaComponent = ({ title, name, description="Corecare is the best cleaning
 
     return (
         <Head>
+
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="manifest" href="/site.webmanifest" />
+            <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+            <meta name="msapplication-TileColor" content="#da532c" />
+            <meta name="theme-color" content="#ffffff" />
+
+
+
             <meta name="robots" content="all" />
             <meta property="og:site_name" content="CoreCare" />
             <meta property="og:url" content={url}/>
@@ -11,7 +22,7 @@ const MetaComponent = ({ title, name, description="Corecare is the best cleaning
             <link rel="image_src" href="" />
             <meta property="og:image" itemProp="image primaryImageOfPage" content="/android-chrome-512x512.png" />
             <meta name="robots" content="follow" />
-            <meta name="author" content="John Doe" />
+            <meta name="author" content="Afzal Saiyed" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <meta name="keywords" content={name}></meta>
             <meta name="description" content={description} />
@@ -20,10 +31,9 @@ const MetaComponent = ({ title, name, description="Corecare is the best cleaning
             <meta name="twitter:creator" content="@Corecare" />
             <meta name="twitter:title" property="og:title" itemProp="name" content={title} />
             <meta name="twitter:description" property="og:description" itemProp="description" content={description} />
-            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-            <link rel="manifest" href="/site.webmanifest.json"></link>
+          
+
+
             <link rel="icon" href="/favicon.ico" />
             <title>{title}</title>
         </Head>
